93836df5c4d7c99d004a018db6de34a22b6cccef
Mochamad Deva <dravamariz@gmail.com>
2024-11-27 22:11:01 +0700

    user

--- database/migrations/2024_11_27_104144_add_user_type_to_users_table.php
+++ database/migrations/2024_11_27_104144_add_user_type_to_users_table.php
@@ -4,25 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->string('user_type')->default('user');
+            $table->rememberToken();
+            $table->timestamps(); // Ini akan menambahkan kolom created_at dan updated_at
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('user_type');
-        });
+        Schema::dropIfExists('users');
     }
-};
+}
